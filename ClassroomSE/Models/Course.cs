57cfc568ee04f31ddb12e8c2576a411ba41538bf
Danykee <dan.eu42@yahoo.com>
2020-06-29 15:28:42 +0300

    0.2
    +Models

---
+++ ClassroomSE/Models/Course.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ClassroomSE.Models
+{
+    public class Course
+    {
+        public Guid CourseID { get; set; }
+        public string CourseTitle { get; set; }
+        public string CourseContent { get; set; }
+        public Class Class { get; set; }
+    }
+}
